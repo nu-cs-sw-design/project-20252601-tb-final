5e41c16175252984c75b3e43a415489b8aae170d
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-12-06 21:22:21 -0600

    implemented Shuffle

---
+++ src/main/java/domain/game/ShuffleAction.java
@@ -0,0 +1,30 @@
+package domain.game;
+
+// ShuffleAction implements shuffle card
+public class ShuffleAction implements CardAction {
+    @Override
+    public void execute(Game game, UIInteraction ui) {
+        ui.displayMessage("decidedShuffle");
+
+        int numberOfShuffle;
+        final int maxNumberOfShuffles = 100;
+        while (true) {
+            ui.displayMessage("enterShuffleTimes");
+            try {
+                numberOfShuffle = ui.getUserInputInteger();
+                if (numberOfShuffle > maxNumberOfShuffles) {
+                    ui.displayMessage("maxShuffleMessage");
+                }
+                else if (numberOfShuffle > 0) {
+                    break;
+                } else {
+                    ui.displayMessage("enterPositiveInteger");
+                }
+            } catch (Exception e) {
+                ui.displayMessage("enterInteger");
+                ui.getUserInputToken();
+            }
+        }
+        game.playShuffle(numberOfShuffle);
+    }
+}
